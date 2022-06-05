3411805e93a109e28b8f017ac2889806cc84a249
DillonB07 <dillonbarnes07@gmail.com>
2022-06-05 13:18:57 +0100

    Fix small bug with spark line chart

--- src/components/Charts/SparkLineChart.jsx
+++ src/components/Charts/SparkLineChart.jsx
@@ -5,36 +5,33 @@ import {
   SparklineTooltip,
 } from "@syncfusion/ej2-react-charts";
 
-const SparkLineChart = ({
-  id,
-  height,
-  width,
-  color,
-  data,
-  type,
-  currentColor,
-}) => {
-  return (
-    <SparklineComponent
-      id={id}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType="Numeric"
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      dataSource={data}
-      xName="x"
-      yName="yval"
-      type={type}
-      tooltipSettings={{
-        visible: true,
-        format: "${x} : data ${yval}",
-        trackLineSettings: { visible: true },
-      }}
-    >
-      <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  );
-};
+class SparkLineChart extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+        tooltipSettings={{
+          visible: true,
+          format: "${x} : data ${yval}",
+          trackLineSettings: { visible: true },
+        }}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
+}
+
 export default SparkLineChart;
